29d0f10ec0611d0cb97c7c632aa81234cc7be65e
Samson Koelle <samsonkoelle@Samsons-MacBook-Pro-2.local>
2024-07-03 11:21:46 -0700

    save

--- convexlocalisometry/experiments.py
+++ convexlocalisometry/experiments.py
@@ -5,6 +5,48 @@ from .loss import isometry_loss
 from .algorithms import greedy, brute
 
 
+def run_experiment(matrix, target_dimension, lambdas=np.logspace(1e-6, 1e-3)):
+    # NOTE (Sam): target dimension is assumed known
+    greedy_selection = greedy(
+        matrix=matrix, target_dimension=target_dimension, loss=isometry_loss
+    )
+    brute_selection = brute(
+        matrix=matrix, target_dimension=target_dimension, loss=isometry_loss
+    )
+
+    #
+    greedy_basis_pursuit_selection = greedy(
+        matrix=matrix,
+        target_dimension=target_dimension,
+        loss=basis_pursuit_loss,
+    )
+
+    #
+    brute_basis_pursuit_selection = brute(
+        matrix=matrix,
+        target_dimension=target_dimension,
+        loss=basis_pursuit_loss,  # This isn't quite right.  I was thinking to do this on the dual.  Does it make sense here? % Direct minimization of this norm has nothing to do with correlation.
+    )
+
+    # convex basis pursuit (this should give the same answer as brute_basis_pursuit_selection) since the problem is convex
+    brute_basis_pursuit_selection = isometric_basis_pursuit(
+        matrix=matrix
+    )  # look at rank since more than D functions selected
+
+    # Two stage basis_pursuit_selection
+    brute_two_stage_selection = brute(
+        matrix=matrix[brute_basis_pursuit_selection],
+        target_dimension=target_dimension,
+        loss=isometry_loss,
+    )  # could also use the basis_pursuit_loss here
+
+    for lambda_ in lambdas:
+        lasso_selection = isometric_lasso(matrix=matrix, lambda_=lambda_)
+
+    # How many functions are retained?
+    # compute angle of the two spaces (its not the angle but kinda... frob norm of projection)
+
+
 def run_unitary_experiment(
     ambient_dimension=10, unitary_dimension=5, noise_dimension=50
 ):
@@ -14,42 +56,4 @@ def run_unitary_experiment(
         unitary_dimension=unitary_dimension,
         noise_dimension=noise_dimension,
     )
-    # NOTE (Sam): target dimension is assumed known
-    greedy_selection = greedy(
-        matrix=unitary_matrix, target_dimension=unitary_dimension, loss=isometry_loss
-    )
-    brute_selection = brute(
-        matrix=unitary_matrix, target_dimension=unitary_dimension, loss=isometry_loss
-    )
-
-    #
-    greedy_basis_pursuit_selection = greedy(
-        matrix=unitary_matrix,
-        target_dimension=unitary_dimension,
-        loss=basis_pursuit_loss,
-    )
-
-    #
-    brute_basis_pursuit_selection = brute(
-        matrix=unitary_matrix,
-        target_dimension=unitary_dimension,
-        loss=basis_pursuit_loss,
-    )
-
-    # convex basis pursuit (this should give the same answer as brute_basis_pursuit_selection) since the problem is convex
-    brute_basis_pursuit_selection = isometric_basis_pursuit(
-        matrix=unitary_matrix
-    )  # look at rank since more than D functions selected
-
-    # Two stage basis_pursuit_selection
-    brute_two_stage_selection = brute(
-        matrix=unitary_matrix[brute_basis_pursuit_selection],
-        target_dimension=unitary_dimension,
-        loss=isometry_loss,
-    )  # could also use the basis_pursuit_loss here
-
-    lambdas = np.logspace(1e-6, 1e-3)
-    for lambda_ in lambdas:
-        lasso_selection = isometric_lasso(matrix=unitary_matrix, lambda_=lambda_)
-
-    # compute angle of the two spaces (its not the angle but kinda... frob norm of projection)
+    run_experiment(matrix=unitary_matrix, target_dimension=unitary_dimension)
--- convexlocalisometry/loss.py
+++ convexlocalisometry/loss.py
@@ -6,3 +6,16 @@ def isometry_loss(matrix: np.ndarray) -> float:
     singular_values = np.linalg.svd(matrix, compute_uv=False)
     squared_loss = np.linalg.norm(np.log(singular_values)) ** 2
     return squared_loss
+
+
+def subspace_loss(matrix_1: np.ndarray, matrix_2: np.ndarray) -> float:
+    """Computes the frobenius norm of the projection of the two matrices"""
+    output = np.linalg.norm(np.einsum("ij, jk -> ik", matrix_1, matrix_2)) ** 2
+    return output
+
+
+def group_basis_pursuit_loss(beta):  # beta_bp
+    """Computes the group basis pursuit loss of the matrix beta"""
+    output = np.linalg.norm(beta, axis=1).sum()
+
+    return output
