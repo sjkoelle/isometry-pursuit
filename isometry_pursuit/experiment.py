bf0019f7cce5f5ae86c5905bee3c45edd4522aed
Samson Koelle <samsonkoelle@Samsons-MacBook-Pro-2.local>
2024-11-25 17:55:55 -0800

    let him cook

--- isometry_pursuit/experiment.py
+++ isometry_pursuit/experiment.py
@@ -1,4 +1,5 @@
 from math import comb
+import time
 
 import pandas as pd
 import numpy as np
@@ -12,19 +13,26 @@ def analyze_data(X, compute_brute=False, power=1.0, limit=1e9):
     D, P = X.shape
     isometry_loss_power = lambda x: isometry_loss(x, power)
     group_brute_loss = lambda x: group_lasso_norm(np.linalg.inv(x))
+    start_greedy = time.time()
     output = greedy(X, isometry_loss_power, D, [])
+    end_greedy = time.time()
     loss = isometry_loss_power(X[:, output])
 
     data_transformed = exponential_transformation(X, power=power)
+    start_basis_pursuit = time.time()
     beta = group_basis_pursuit(data_transformed)
+    end_basis_pursuit = time.time()
     basis_pursuit_indices = np.where(np.linalg.norm(beta, axis=1))[0]
     nbp = len(basis_pursuit_indices)
     brute_complexity = comb(nbp, D)
     print(f"Brute force complexity {brute_complexity} from D={D} and nbp={nbp}")
     if brute_complexity <= limit:
+        start_two_stage = time.time()
         two_stage_output = basis_pursuit_indices[
             np.asarray(brute(X[:, basis_pursuit_indices], isometry_loss_power, D))
         ]  # plainly this is too hard 178**13 combinations
+        end_two_stage = time.time()
+        stage_two_time = end_two_stage - start_two_stage
         two_stage_loss = isometry_loss_power(X[:, two_stage_output])
 
         two_stage_multitask = basis_pursuit_indices[
@@ -45,6 +53,7 @@ def analyze_data(X, compute_brute=False, power=1.0, limit=1e9):
         two_stage_loss = np.nan
         random_two_stage_loss = np.nan
         greedy_multitask_norm_two_stage = np.nan
+        stage_two_time = np.nan
 
     if compute_brute:
         if brute_complexity > limit:
@@ -69,6 +78,9 @@ def analyze_data(X, compute_brute=False, power=1.0, limit=1e9):
         greedy_multitask_norm_two_stage,
         brute_loss,
         brute_isometry_loss,
+        end_basis_pursuit - start_basis_pursuit,
+        stage_two_time,
+        end_greedy - start_greedy,
     )
 
 
@@ -81,6 +93,9 @@ def run_resampling_experiment(data, D, frac=0.5, R=25, compute_brute=False, powe
     greedy_multitask_norms_two_stage = []
     brute_isometry_losses = []
     brute_losses = []
+    greedy_times = []
+    basis_pursuit_times = []
+    stage_two_times = []
 
     for i in range(R):
         np.random.seed(i)
@@ -94,6 +109,9 @@ def run_resampling_experiment(data, D, frac=0.5, R=25, compute_brute=False, powe
             greedy_multitask_norm_two_stage,
             brute_loss,
             brute_isometry_loss,
+            basis_pursuit_time,
+            stage_two_time,
+            greedy_time,
         ) = analyze_data(X, compute_brute=compute_brute, power=power)
         losses.append(loss)
         support_cardinalities_basis_pursuit.append(support_cardinality_basis_pursuit)
@@ -102,7 +120,9 @@ def run_resampling_experiment(data, D, frac=0.5, R=25, compute_brute=False, powe
         random_two_stage_losses.append(random_two_stage_loss)
         brute_losses.append(brute_loss)
         brute_isometry_losses.append(brute_isometry_loss)
-
+        basis_pursuit_times.append(basis_pursuit_time)
+        stage_two_times.append(stage_two_time)
+        greedy_times.append(greedy_time)
     # Creating the dataframe
     results_df = pd.DataFrame(
         {
@@ -113,6 +133,10 @@ def run_resampling_experiment(data, D, frac=0.5, R=25, compute_brute=False, powe
             "Greedy Multitask Norms (Two-Stage)": greedy_multitask_norms_two_stage,
             "Brute Isometry Losses": brute_isometry_losses,
             "Brute Losses": brute_losses,
+            "Basis Pursuit Times": basis_pursuit_times,
+            "Stage Two Times": stage_two_times,
+            "Greedy Times": greedy_times,
         }
     )
+
     return results_df
